97f863dd46ef4a08bab0ac5f79bb6664adebd29f
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-05-10 09:59:04 +0530

    learned fs module and blocking vs non-blocking operations

--- Folder1/file.js
+++ Folder1/file.js
@@ -1,5 +1,5 @@
-const { log } = require('console');
-const fs = require('fs');
+// const { log } = require('console');
+// const fs = require('fs');
 
 //Sync
 // fs.writeFileSync('./test.txt','Hello World');
@@ -24,4 +24,27 @@ const fs = require('fs');
 // fs.unlinkSync("./copy.txt");
 // console.log(fs.statSync("./test.txt"));
 // console.log(fs.statSync("./test.txt").isFile());
-fs.mkdirSync('my-docs')
+// fs.mkdirSync('my-docs')
+
+const fs = require("fs");
+const os = require("os");
+
+console.log(os.cpus().length)
+
+// console.log("1");
+
+//Blocking...
+// const result = fs.readFileSync('contacts.txt', 'utf-8');
+// console.log(result);
+
+//Non-Blocking...
+// const result = fs.readFile('contacts.txt', 'utf-8',(err,result)=>{
+//     // console.log(result);
+// });
+
+// console.log("2");
+// console.log("3");
+// console.log("4");
+
+//Default Thread Pool Size = 4
+// Max? = 8core CPU -> 8

e51451d411f675f73b7290536c09d563cfdc9d50
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-05-12 22:56:21 +0530

    Created Express server with users API routes

---
+++ Project-01/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const users = require('./MOCK_DATA.json')
+
+const app = express();
+const PORT = 8000;
+
+app.get('/users' , (req ,res) => {
+   const html = `
+   <ul>
+    ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+   </ul>
+   `;
+   res.send(html);
+})
+
+//REST API 
+app.get('/api/users', (req ,res) => {
+    return res.json(users);
+})
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`))

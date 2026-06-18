1de1798d10e92b8fc76d27b6c53530c69d0b8475
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-06-18 10:56:08 +0530

    File handling in nodeJS

--- Folder2/file.js
+++ Folder2/file.js
@@ -1,3 +1,31 @@
 // const math = require("./math");
 // console.log(math);
 // Modules
+
+const fs = require("fs");
+
+//Sync...
+// fs.writeFileSync('./test.txt', 'HEy there');
+
+//Async...
+// fs.writeFile("./test.txt", "Hello Wolrd Async", (err) => {});
+
+// const result = fs.readFileSync("./contacts.txt", "utf-8");
+// console.log(result);
+
+// fs.readFile("./contacts.txt", "utf-8", (err, result) => {
+//     if(err){
+//         console.log('Error', err);
+//     }else{
+//         console.log(result);
+//     }
+// })
+
+// fs.appendFileSync("./test.txt", new Date().getDate().toLocaleString());
+// fs.appendFileSync("./test.txt", 'Hey There\n');
+
+
+// fs.unlinkSync("./contacts.txt");
+
+// console.log(fs.statSync("./test.txt"));
+// fs.mkdirSync('my-docs/a', {recursive: true});

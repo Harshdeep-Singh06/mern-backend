671bfda94c3858a11834cb4601ac33e0f51eeea7
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-07-14 19:00:05 +0530

    HTTP Methods

--- server2/index.js
+++ server2/index.js
@@ -5,14 +5,25 @@ const myServer = http.createServer((req, res)=>{
     // console.log(req.headers)
     // console.log(req)
     // console.log("New req recieve");
-    const log = `${Date.now()}: ${req.url} New Req Recieved\n`
+    const log = `${Date.now()}: ${req.method} ${req.url} New Req Recieved\n`
     fs.appendFile('log.txt',log,(err, data)=>{
         switch(req.url){
-            case '/': res.end("Homepage");
+            case '/': 
+            if(req.method === 'GET') res.end("Homepage");
             break
             case '/about': res.end("Hey I am Batman")
             break
-            
+            case "/search":
+                const search = myUrl.query.search_query;
+                res.end("Here are you result for" + search)
+                break
+            case '/SignUp':
+            if(req.method === 'GET') res.end("This is a sign up form")
+            else if(req.method === 'POST'){
+                    // DB Query
+                    res.end("Success")
+            }
+                break;
             default:
                 res.end("4O4 Not Found")
         }

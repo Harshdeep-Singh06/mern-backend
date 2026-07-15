e7e5ad9292fb049a42a7bdedc05d71eb3e2d0e06
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-07-15 19:22:14 +0530

    Getting started with node js

--- server2/index.js
+++ server2/index.js
@@ -1,34 +1,13 @@
-const http = require("http")
-const fs = require('fs');
+const express = require('express');
 
-const myServer = http.createServer((req, res)=>{
-    // console.log(req.headers)
-    // console.log(req)
-    // console.log("New req recieve");
-    const log = `${Date.now()}: ${req.method} ${req.url} New Req Recieved\n`
-    fs.appendFile('log.txt',log,(err, data)=>{
-        switch(req.url){
-            case '/': 
-            if(req.method === 'GET') res.end("Homepage");
-            break
-            case '/about': res.end("Hey I am Batman")
-            break
-            case "/search":
-                const search = myUrl.query.search_query;
-                res.end("Here are you result for" + search)
-                break
-            case '/SignUp':
-            if(req.method === 'GET') res.end("This is a sign up form")
-            else if(req.method === 'POST'){
-                    // DB Query
-                    res.end("Success")
-            }
-                break;
-            default:
-                res.end("4O4 Not Found")
-        }
-    })
-});
+const app = express();
 
-myServer.listen(8000, ()=>console.log("Server Started"))
+app.get('/', (req,res)=>{
+    return res.send('Hello From Home page')
+})
 
+app.get('/about',(req,res)=>{
+    return res.send(`Hello ${req.query.name}`);
+})
+
+app.listen(8000, () => console.log("Server Started!"))

9483874b17d892cabd65b7a6469a8d74a12f3c31
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-07-21 18:30:27 +0530

    Build REST API's using Node and Express.js

--- Project-02/index.js
+++ Project-02/index.js
@@ -5,8 +5,41 @@ const PORT = 8000;
 
 //ROUTES
 
+app.get('/users', (req,res)=>{
+    const html = `
+    <ul>
+     ${users.map(user=>`<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html)
+})
+
+//REST API
+
 app.get('/api/users', (req,res)=>{
     return res.json(users);
 })
+
+app.route("/api/users/:id")
+.get((req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=> user.id === id);
+    return res.json(user);
+})
+.patch((req,res)=>{
+    //Edit user with id
+    return res.json({status:"pending"})
+})
+.delete((req,res)=>{
+    //Delete user with id
+    return res.json({status:"pending"})
+})
+
+app.post("/api/users",(req,res)=>{
+    // Add new user
+    return res.json({status:"Pending"})
+})
+
+
 
 app.listen(PORT,()=>console.log(`Server started at PORT:${PORT}`))

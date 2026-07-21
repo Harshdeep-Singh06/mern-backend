17de33e5a594dcd0a6dd645940901343b98860aa
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-07-21 13:39:32 +0530

    Create new project

---
+++ Project-02/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const users = require("./MOCK_DATA.json");
+const app = express()
+const PORT = 8000;
+
+//ROUTES
+
+app.get('/api/users', (req,res)=>{
+    return res.json(users);
+})
+
+app.listen(PORT,()=>console.log(`Server started at PORT:${PORT}`))
